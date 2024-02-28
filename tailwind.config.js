e40efe63f80d302ad5b22c445cbf0d6187b995da
Sanjeev18207 <u.sanjeev.23cse@psvpec.in>
2024-02-28 10:38:28 +0530

    design: landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Softred: "#fe7867",
+        Yellow: "#fad400",
+        Darkdesaturatedcyan: "#25564b",
+        Darkblue: "#19536b",
+        Darkmoderatecyan: "#458c7e",
+        Verydarkdesaturatedblue: "#23303e",
+        Verydarkgrayishblue: "#5a636c",
+        Darkgrayishblue: "#818498",
+        Grayishblue: "#a7abae",
+      },
+    },
+  },
+  plugins: [],
+};
